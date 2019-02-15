8a522b39cb730cbf2033478e39790a0b1ca656d8
EugenyB <eberkunsky@gmail.com>
2019-02-15 19:31:13 +0200

    final version

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,40 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	    new Main().run();
+	    new Main().runUpdate();
+    }
+
+    private void runUpdate() {
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3306/mainacaddemo1",
+                "eugeny",
+                "123")) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select * from person where name = ?");
+            preparedStatement.setString(1, "Qwerty");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                //String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double salary = resultSet.getDouble("salary");
+                System.out.println("Новое имя для : " + "Qwerty");
+                String name = new Scanner(System.in).next();
+
+                PreparedStatement updateStatement = connection.prepareStatement(
+                        "update person set name=?, age=?, salary=? where id=?");
+                updateStatement.setInt(4, id);
+                updateStatement.setString(1, name);
+                updateStatement.setInt(2, age);
+                updateStatement.setDouble(3, salary);
+                updateStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private void run() {
